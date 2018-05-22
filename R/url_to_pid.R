04b07aa970961c51415cfcac4a595fe2011944cf
Irene <25118334+isteves@users.noreply.github.com>
2018-05-22 10:14:23 -0700

    added check_version (runs getSystemMetadata and provides message)

--- R/url_to_pid.R
+++ R/url_to_pid.R
@@ -37,7 +37,41 @@ url_to_pid <- function(url){
     
     if(nrow(pids) > 1){
         warning("Multiple data pids were matched.")
+        
+        # It seems that on PASTA, data objects get slightly new pids (/1/, /2/, etc)
+        # when the metadata is updated
     }
     
     return(pids)
+}
+
+#' Check pid version
+#'
+#' This function takes a pid and checks to see if it has been obsoleted.
+#'
+#' @param pid (character) The pid to a data, metadata, or resource map object on a DataONE member node
+#'
+#' @importFrom dataone CNode getSystemMetadata
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'    pid <- "https://pasta.lternet.edu/package/data/eml/edi/195/2/51abf1c7a36a33a2a8bb05ccbf8c81c6"
+#'    pid <- "doi:10.18739/A2HF7Z"
+#'    
+#'    check_version(pid)
+#' }
+#'
+
+check_version <- function(pid){
+    sysmeta <- dataone::getSystemMetadata(dataone::CNode("PROD"), pid)
+    
+    if(is.na(sysmeta@obsoletedBy)){
+        print("This is the latest version of the pid.")
+    } else {
+        print(paste("The pid has been obsoleted by", sysmeta@obsoletedBy))
+    }
+    
+    #TODO: check metadata?
 }
